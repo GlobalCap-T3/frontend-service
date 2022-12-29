aaa6fe957d93c1cea8a482c50a9bb9f32bdb5ec8
ybuser <ybuser@naver.com>
2022-12-29 20:02:26 +0900

    feat: discussion page designing, no api yet

---
+++ src/components/discussion/SearchSidebar.js
@@ -0,0 +1,152 @@
+import React, { useState, useEffect } from "react";
+import { useNavigate, Navigate } from "react-router-dom"
+import styled from "styled-components"
+import temp_profile from "../../images/temp_profile.png";
+
+const exampleSearchedDiscussions = [
+  {
+    title:"searched title0",
+    content:"searched content0",
+    user_id:"0",
+    discussion_id:"10",
+  },
+  {
+    title:"searched title1",
+    content:"searched content1",
+    user_id:"1",
+    discussion_id:"11",
+  },
+  {
+    title:"searched title2",
+    content:"searched content2",
+    user_id:"2",
+    discussion_id:"12",
+  },
+  {
+    title:"searched title3",
+    content:"searched content3",
+    user_id:"3",
+    discussion_id:"13",
+  },
+  {
+    title:"searched title4",
+    content:"searched content4",
+    user_id:"4",
+    discussion_id:"14",
+  },
+  {
+    title:"searched title5",
+    content:"searched content5",
+    user_id:"5",
+    discussion_id:"15",
+  },
+  {
+    title:"searched title6",
+    content:"searched content6",
+    user_id:"6",
+    discussion_id:"16",
+  },
+  {
+    title:"searched title7",
+    content:"searched content7",
+    user_id:"7",
+    discussion_id:"17",
+  },
+  {
+    title:"searched title8",
+    content:"searched content8",
+    user_id:"8",
+    discussion_id:"18",
+  },
+  {
+    title:"searched title9",
+    content:"searched content9",
+    user_id:"9",
+    discussion_id:"19",
+  },
+];
+
+const Sidebar = styled.div`
+  width: 25%;
+  min-width: 150px;
+  max-width: 300px;
+  height: 100%;
+  background-color: lightgrey;
+  overflow: auto;
+`;
+
+const SearchbarContainer = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 10px;
+`
+
+const SearchedContainer = styled.ul`
+  list-style-type: none;
+`;
+
+const SearchedItem = styled.li`
+  padding: 7px 0 7px 0;
+  height: 100px;
+  font-size: 15px;
+  cursor: pointer;
+  // background-color: ${(props) => (props.selected ? 'white' : 'none')};
+  &:hover{
+    background-color: white;
+  }
+`;
+
+export default function SearchSidebar(props) { // props : onDiscussionClicked
+  const navigate = useNavigate();
+  const [currentChannel, setCurrentChannel] = useState(props.currentChannel); // Information, Published Paper, Following, Notes/Documents, Message
+  const [profileImage, setProfileImage] = useState(props.profileImage); // profile pic src
+  const [searchedDiscussions, setSearchedDiscussions] = useState([]);
+
+  const getSearchedDiscussion = async() => {
+    // (need) code for fetching all searched discussions
+
+    // under is temp code
+    let tempSearchedDiscussions = searchedDiscussions;
+    tempSearchedDiscussions = exampleSearchedDiscussions;
+    setSearchedDiscussions(tempSearchedDiscussions);
+  };
+
+
+  const onSearchClicked = (e) => {
+    console.log("search discussion with keyword ", e.target);
+    getSearchedDiscussion();
+  };
+
+  const onDiscussionClicked = (e, key) => {
+    console.log("key is ", key);
+    navigate(`/discussion/${key}`);
+  };
+
+  const onPageButtonClicked = (e) => {
+    console.log("current page is ");
+    console.log("changing page is ");
+    // code to change to next/previous page in the searched discussions
+  };
+
+  return (
+    <Sidebar>
+      <SearchbarContainer> {/* search bar*/}
+        <button onClick={onSearchClicked}>Search</button>
+      </SearchbarContainer>
+      <div> {/* searched discussion list (pagination by 10) */}
+        <SearchedContainer>
+          {searchedDiscussions.map(function(value, index){ // (need) pagination
+            return (
+              <SearchedItem key={index} style={{"border":"5px solid black"}} onClick={e => onDiscussionClicked(e, value.discussion_id)}>
+                <div>Title: {value.title}</div>
+                <div>User ID: {value.user_id}</div>
+              </SearchedItem>
+            )
+          })}
+        </SearchedContainer>
+      </div>
+    </Sidebar>
+  );
+}
--- src/pages/Discussion.js
+++ src/pages/Discussion.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react"
 import { Link, useNavigate, Navigate, useParams } from "react-router-dom"
 import authenClient from "../clients/authenClient"
 import { useUserContext } from "../context/user"
-import LeftBar from "../components/profile/LeftBar"
+import SearchSidebar from "../components/discussion/SearchSidebar"
 import Information from "../components/profile/Information"
 import styled from "styled-components"
 
@@ -10,70 +10,111 @@ const exampleRecentDiscussions = [
   {
     title:"title0",
     content:"content0",
-    user_id:"0"
+    user_id:"0",
+    discussion_id:"0",
   },
   {
     title:"title1",
     content:"content1",
-    user_id:"1"
+    user_id:"1",
+    discussion_id:"1",
   },
   {
     title:"title2",
     content:"content2",
-    user_id:"2"
+    user_id:"2",
+    discussion_id:"2",
   },
   {
     title:"title3",
     content:"content3",
-    user_id:"3"
+    user_id:"3",
+    discussion_id:"3",
   },
   {
     title:"title4",
     content:"content4",
-    user_id:"4"
+    user_id:"4",
+    discussion_id:"4",
   },
   {
     title:"title5",
     content:"content5",
-    user_id:"5"
+    user_id:"5",
+    discussion_id:"5",
   },
   {
     title:"title6",
     content:"content6",
-    user_id:"6"
+    user_id:"6",
+    discussion_id:"6",
   },
   {
     title:"title7",
     content:"content7",
-    user_id:"7"
+    user_id:"7",
+    discussion_id:"7",
   },
   {
     title:"title8",
     content:"content8",
-    user_id:"8"
+    user_id:"8",
+    discussion_id:"8",
   },
   {
     title:"title9",
     content:"content9",
-    user_id:"9"
+    user_id:"9",
+    discussion_id:"9",
   },
 ];
 
 const Container = styled.div`
   display: flex;
-  height: 100vh;
+  justify-content: space-between;
+  position: relative;
+  height: auto;
+  min-height: 100%;
 `;
+
 const InnerContainer = styled.div`
   display: block;
+  overflow: auto;
   width: 100%;
+  align-items: center;
+  justify-content: center;
   padding: 0 10px 0 10px;
 `;
+
+const EventsSidebar = styled.div`
+  width: 25%;
+  min-width: 150px;
+  max-width: 300px;
+  height: 100%;
+  background-color: lightgrey;
+`;
+
+const DiscussionPreview = styled.div`
+  max-width: 800px;
+  height: auto;
+  min-height: 100px;
+  max-height: 200px;
+  border-radius: 20px;
+  margin-top: 20px;
+  margin-left: auto;
+  margin-right: auto;
+
+  color: white;
+  background-color: #737373;
+`
+
+
 
 export default function Discussion() {
   const navigate = useNavigate();
   const params = useParams(); // {discussionId: '123'}
 
-  const [recentDiscussions, setRecentDiscussions] = useState([]); // Information, Published Paper, Following, Notes/Documents, Message
+  const [recentDiscussions, setRecentDiscussions] = useState([]);
 
 
   const getDiscussion = async() => {
@@ -99,38 +140,42 @@ export default function Discussion() {
   return ( 
     <Container>
       {/* Left side bar for discussion search */}
+      <SearchSidebar onDiscussionClicked={onDiscussionClicked} />
 
       {/* If URL has a id, show the specific discussion */}
-
-      {/* Make new post */}
       {/* Recent discussions */}
       {(params.discussionId)?(
-        <div onClick={() => navigate(-1)} style={{"border":"5px solid black"}}>
+        <InnerContainer onClick={() => navigate(-1)} style={{"border":"5px solid black"}}>
           Discussion thread no.{params.discussionId}
           <div>
             back
           </div>
-        </div>
+        </InnerContainer>
       ):(
-        <div style={{"display":"block"}}>
+        <InnerContainer>
           Main Discussion Page
+          
+          {/* Make new post */}
+          <div> Make new Post </div>
+
           <div>
             Recent Discussions
           </div>
-          <></>
           {recentDiscussions.map(function(value, index){
             return (
-              <div key={index} style={{"border":"5px solid black"}} onClick={e => onDiscussionClicked(e, index)}>
-                Title: {recentDiscussions[index].title}
-                Content: {recentDiscussions[index].content}
-                User ID: {recentDiscussions[index].user_id}
-              </div>
+              <DiscussionPreview key={index} onClick={e => onDiscussionClicked(e, value.discussion_id)}>
+                <div>Title: {value.title}</div>
+                <div>User ID: {value.user_id}</div>
+                
+                <div>Content: {value.content}</div>
+              </DiscussionPreview>
             )
-          })}
-          
+          })}          
 
-        </div>
+        </InnerContainer>
       )}
+      
+      <EventsSidebar></EventsSidebar>
 
     </Container>
   );
